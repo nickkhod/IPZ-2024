7f54709fd7b315a45803ce35439b964758bd054d
Semenova Valeriia <lerasem47@gmail.com>
2025-02-09 20:37:05 +0100

    modified Task 3

--- IPZ-33/15_Valeriia/Task 3 /menu_data.py
+++ IPZ-33/15_Valeriia/Task 3 /menu_data.py
@@ -1,8 +1,13 @@
-menu = [
-    {"id": 1, "name": "Margherita Pizza", "category": "Pizza", "price": 150},
-    {"id": 2, "name": "Pepperoni Pizza", "category": "Pizza", "price": 180},
-    {"id": 3, "name": "Chicken Burger", "category": "Burgers", "price": 120},
-    {"id": 4, "name": "Cheeseburger", "category": "Burgers", "price": 130},
-    {"id": 5, "name": "Caesar Salad", "category": "Salads", "price": 100},
-    {"id": 6, "name": "Borsch", "category": "Soups", "price": 80},
-]
+import json
+
+# Шлях до файлу з меню
+MENU_FILE = "menu.json"
+
+def load_menu():
+    try:
+        with open(MENU_FILE, "r", encoding="utf-8") as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+menu = load_menu()
